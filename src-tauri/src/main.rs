f5a33715fab55a9854c5905e9c45b8cefbd00154
bad <soccerskin87@gmail.com>
2022-12-11 21:34:47 +0000

    Initial commit to this branch

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,9 +10,29 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    std::thread::spawn(move || {
+        if let Err(error) = listen(callback) {
+            println!("Error: {:?}", error)
+        }
+    });
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
+use rdev::{listen, Event, EventType};
+
+// This will block.
+
+import { appWindow, WebviewWindow } from '@tauri-apps/api/window'
+fn callback(event: Event) {
+    println!("My callback {:?}", event);
+    match event.event_type {
+        EventType::KeyPress(key) => println!("Key pressed: {:?}", key),
+        app.emit_all("event-name", Payload { message: "Tauri is awesome!".into() }).unwrap();
+        EventType::KeyRelease(key) => println!("Key released: {:?}", key),
+        _ => (),
+    }
+}
